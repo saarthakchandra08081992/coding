8b8981c8f3267058b886fab608a356bbcabd4ee6
sc2776 <sc2776@cornell.edu>
2016-11-14 18:43:36 -0500

    Adding these mods

--- src/CollectionExample.java
+++ src/CollectionExample.java
@@ -1,14 +1,28 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CollectionExample {
 
 	
 	public static void main(String[] args) {
-		String buf = new String("Saarthak,  Chandra");
-		List<String> list = Arrays.asList(buf.split(","));
+		String buf = new String("  Saarthak,  Chandra is   good");
+		List<String> list = Arrays.asList(buf.trim().split(","));
+		printStrings(list);
 		
+		Pattern pat = Pattern.compile("([a-z|A-Z]+)");
+		Matcher mat = pat.matcher(buf);
+		
+		System.out.println();
+		while(mat.find()){
+			System.out.println("Matched is  - "+mat.group(0));
+		}
+	}
+	
+	public static void printStrings(List<String> list){
+		for(String str : list){
+			System.out.print(str);
+		}
 	}
 }
